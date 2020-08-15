e24c3979554b86c5340b547d5c57b498cb09e1c4
tiffany <tiffany@tiffanyui-MacBook-Pro.local>
2020-08-15 19:09:02 +0900

    add docs

---
+++ docs/precache-manifest.b0a3c3fd9afe4ac46ad7ee4db55ac38e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b27d3bffaa07ba044b29",
+    "url": "/css/app.bdd38446.css"
+  },
+  {
+    "revision": "d554eaf94ffc21b0b3ec",
+    "url": "/css/chunk-vendors.496dd37e.css"
+  },
+  {
+    "revision": "1f8e8a80250dd0b8c241c21f606d8765",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f932e4978189d73416d",
+    "url": "/js/about.5f86a53c.js"
+  },
+  {
+    "revision": "b27d3bffaa07ba044b29",
+    "url": "/js/app.bfba0079.js"
+  },
+  {
+    "revision": "d554eaf94ffc21b0b3ec",
+    "url": "/js/chunk-vendors.c1410065.js"
+  },
+  {
+    "revision": "0cea38f23616aaf0b609217bafc39c84",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
